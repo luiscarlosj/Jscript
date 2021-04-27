74ef09aedb8a6614a15583a746d06f50083ef881
luiscarlos <luiscarlossousaj@gmail.com>
2021-04-26 23:02:49 -0300

    condições if else

---
+++ aula11/exc08.js
@@ -0,0 +1,23 @@
+console.log('o console funcionou corretamente')
+
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+console.log(`Dirija sempre com segurança`)
+
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida!`)
+}
+
+
+var país = 'EUA'
+
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil'){
+    console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}
+
