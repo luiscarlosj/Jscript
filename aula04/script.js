6031909ff9e55dfda2b979d7cd822a3745259c67
luiscarlos <luiscarlossousaj@gmail.com>
2021-05-06 23:29:16 -0300

    variáveis, arrays e objeto no JS

---
+++ aula04/script.js
@@ -0,0 +1,41 @@
+num_int = 1 // Números
+
+num_real = 0.01  // Real
+
+string_aspas = "hello world"   //String
+
+string_aspas_simples = 'JavaScript'    //String
+
+valor_booleanot = true   //Booleane verdadeiro
+
+valor_booleanof = false  //Booleano falso
+
+valor_null = null //null é um valor especial que significa nenhum valor
+
+var primes = [2, 3, 5, 7]
+
+console.log(primes.length) //lenght pega o tamanho do vetor
+
+//Criação de Objetos
+
+var book1={}  //criaçaõ de um objeto vazio
+
+var book = {
+    topic: "JavaScript",
+    fat: true
+};
+
+//Formas de acessar as propriedade do objeto; com "." ou com "[]"
+console.log(book.topic)
+console.log(book.fat)
+
+console.log(book["topic"])
+console.log(book["fat"])
+
+
+
+
+
+
+
+
