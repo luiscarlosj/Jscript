1dadd06e39be79b6aaa78c996c245d05b7caee7e
luiscarlos <luiscarlossousaj@gmail.com>
2021-05-17 23:40:46 -0300

    ajuste mudando imagem

--- Js02/script.js
+++ Js02/script.js
@@ -1,11 +1,11 @@
 var res = document.getElementById('res')
 
 var img = document.createElement('img')
-
 img.setAttribute('id', 'foto')
 
 var num = 0
 var classif=''
+res.style.textAlign = 'center'
 
 function avancar(){
 
@@ -17,27 +17,29 @@ function avancar(){
                 img.setAttribute('src', '1.png')
                 classif = 'número 1 (um)'
                 res.innerHTML+= `Está no número ${num} <br>`
+
             }else if(num==1){ 
                 
                 num++              
                 img.setAttribute('src', '2.png')
                 classif = 'número 2 (dois)'
-                res.innerHTML+= `Está no número ${num} <br>` 
+                res.innerHTML+= `Está no número ${num} <br>`
+
             }else if(num==2){
                 
                 num++
                 img.setAttribute('src', '3.jpg')
                 classif = 'número 3 (três)'
                 res.innerHTML+= `Está no número ${num} <br>`
+
             }else if(num==3){
+
                 res.innerHTML+= `Está no limite ${num} <br>`
                 window.alert(`Não pode mais avançar. Só vai até 3 (três)`)                
             }
-
-       
-        res.style.textAlign = 'center'
-        res.innerHTML+= `${classif}`
+        
         res.appendChild(img)
+        res.innerHTML+= `<br>${classif}`
 }
 
 function retornar(){
@@ -57,14 +59,13 @@ function retornar(){
                 res.innerHTML+= `Está no número ${num} <br>`
 
             }else if(num==3){
+                
                 num--             
                 img.setAttribute('src', '2.png')
                 classif = 'número 2 (dois)'
                 res.innerHTML+= `Está no número ${num} <br>`            
             }
 
-       
-        res.style.textAlign = 'center'
-        res.innerHTML+= `${classif}`
         res.appendChild(img)
+        res.innerHTML+= `<br>${classif}`
 }
